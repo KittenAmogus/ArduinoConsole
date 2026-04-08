74d6fb68d53f4829dc482b8509a249e8847654aa
KittenAmogus <kittenamogus3@gmail.com>
2026-04-08 15:29:43 +0300

    Fix wokwi mode

--- ArduinoConsole.ino
+++ ArduinoConsole.ino
@@ -1,7 +1,11 @@
 #include "config.h"
 #include "utils/display.h"
 
+#ifdef WOKWI_MODE
+DISPLAY_TYPE u8g2(U8G2_R0, /*RES*/ 8);
+#else
 DISPLAY_TYPE u8g2(U8G2_R0, /*CS*/ 10, /*DC*/ 9, /*RES*/ 8);
+#endif
 
 int main(void) {
   init();

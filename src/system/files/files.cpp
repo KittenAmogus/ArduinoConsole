f5e70e552776572d29f734b4102427a2272b20a6
KittenAmogus <kittenamogus3@gmail.com>
2026-04-17 12:16:01 +0300

    Cleared old and stupid code, now i will create normal structure

--- src/system/files/files.cpp
+++
@@ -1,6 +0,0 @@
-#include "files.h"
-
-static void draw();
-static void handleInput();
-static void startFiles();
-
--- src/system/files/files.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILES_H
-#define FILES_H
-
-#include "app/app.h"
-
-#endif // FILES_H
-

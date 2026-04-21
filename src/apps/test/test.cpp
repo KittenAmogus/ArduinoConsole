cefe9618ea3cbbe8a387a0404d3c29ab0a8dbb04
KittenAmogus <kittenamogus3@gmail.com>
2026-04-21 16:37:52 +0300

    Fixed (kinda) fps in test hardware

--- src/apps/test/test.cpp
+++ src/apps/test/test.cpp
@@ -11,42 +11,39 @@ void TestApp::onExit() {
 }
 
 void TestApp::update() {
+  // FPS
+  uint32_t now = millis();
+  uint32_t delta = now - this->appData->lastFrame;
+  this->appData->lastFrame = now;
+
+  if (now - this->appData->lastUpdate > 512) {
+    if (delta > 0) this->appData->fps = 1000 / delta;
+    this->appData->lastUpdate = now;
+  }
 
   sysmem.needRedraw = 1;
 }
 
 void TestApp::draw() {
-  uint32_t frameStart = millis();
-  uint32_t delta = frameStart - this->appData->lastFrame;
-  this->appData->lastFrame = frameStart;
-
-  if (frameStart - this->appData->lastUpdate > 256) {
-    if (delta > 0) this->appData->fps = 1000 / delta;
-    this->appData->lastUpdate = frameStart;
-  }
-
   display.firstPage();
   uint8_t page = 0;
   do {
     if (page == 0) {
-      display.setFont(u8g2_font_u8glib_4_tr); // Smallest readable font
+      display.setFont(u8g2_font_u8glib_4_tr);
       display.setDrawColor(1);
-      display.setCursor(SCREEN_WIDTH - 28 - 2, 5 + 2); // Top right - offset - len("FPS 000")
+      display.setCursor(SCREEN_WIDTH - 30, 7);
       display.print(F("FPS "));
       display.print(this->appData->fps);
     }
 
-    // Update buttons
-    else if (page == 1) {
-      // No system, it cannot be checked
-      for (uint8_t btn=0; btn<6; btn++) {
-        display.setDrawColor(sysmem.buttons.is.mask & (1 << btn));
-        display.drawBox(btn << 4, 16, 16, 16);  // Y = 16, X = 16 * btn
+    for (uint8_t btn=0; btn<6; btn++) {
+      if (sysmem.buttons.is.mask & (1 << btn)) {
+        display.setDrawColor(1);
+        display.drawBox(btn << 4, 16, 16, 16);
       }
     }
 
     page++;
   } while (display.nextPage());
-  this->appData->lastFrame = sysmem.currentTime;
 }
 
